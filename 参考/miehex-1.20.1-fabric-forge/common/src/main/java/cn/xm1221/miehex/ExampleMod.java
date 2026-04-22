57643c86fb84a1aaea08ec6c014372ada3ed18a7
xm1221 <3850632202@qq.com>
2026-04-22 09:10:30 +0800

    try to use arch

---
+++ 参考/miehex-1.20.1-fabric-forge/common/src/main/java/cn/xm1221/miehex/ExampleMod.java
@@ -0,0 +1,9 @@
+package cn.xm1221.miehex;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "miehex";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
