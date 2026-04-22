6e22a80a61cf0e2122d4a0401cbec886a4157d31
xm1221 <3850632202@qq.com>
2026-04-22 09:24:48 +0800

    Revert "try to use arch"
    This reverts commit 57643c86fb84a1aaea08ec6c014372ada3ed18a7.

--- 参考/miehex-1.20.1-fabric-forge/common/src/main/java/cn/xm1221/miehex/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package cn.xm1221.miehex;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "miehex";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
